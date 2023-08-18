cb4cf03880e0149c5074e5b8f45dc58db3f6c51f
Ruk228 <icecreamcrusher21@gmail.com>
2023-08-18 19:35:22 +0400

    fix bug user create subscriptions

--- application/serializers/UserSerializer.py
+++ application/serializers/UserSerializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from application.models import User
+from application.models import User, Blog
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -15,11 +15,17 @@ class UserSerializerСhanges(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
     is_admin = serializers.BooleanField()
     password = serializers.CharField()
+    subscriptions = serializers.PrimaryKeyRelatedField(queryset=Blog.objects.all(), many=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'is_admin', 'password']
+        fields = ['id', 'username', 'is_admin', 'password', 'subscriptions']
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        subscriptions_list = validated_data.pop('subscriptions', [])
+        user = super().create(validated_data)
+
+        if subscriptions_list:
+            user.subscriptions.set(subscriptions_list)
+
         return user
