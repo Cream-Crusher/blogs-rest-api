b2c857587c0695405516d751a0ab4823ee737b75
Cream-Crusher <icecreamcrusher21@gmail.com>
2023-09-07 14:17:22 +0400

    changed unittest post model

--- application/tests/views/test_post.py
+++ application/tests/views/test_post.py
@@ -19,7 +19,8 @@ class PostFilterTest(TestCase):
             title=self.title,
             author=self.user,
         )
-        self.filter_set = PostFilter({'title': self.post}, queryset=Post.objects.all())
+        self.filter_data_created_at = {'created_at_0': '2023-01-01', 'created_at_1': '2023-12-31'}
+        self.queryset = Post.objects.all()
 
     def tearDown(self):
         self.user.delete()
@@ -27,8 +28,17 @@ class PostFilterTest(TestCase):
 
     def test_title_filter(self):
         # act
+        self.filter_set = PostFilter({'title': self.post}, queryset=self.queryset)
         filtered_posts = self.filter_set.queryset
 
         # assert
         self.assertEqual(len(filtered_posts), 1)
         self.assertEqual(filtered_posts[0], self.post)
+
+    def test_created_at_filter(self):
+        # act
+        blog_filter = PostFilter(data=self.filter_data_created_at, queryset=self.queryset)
+        filtered_queryset = blog_filter.queryset
+
+        # assert
+        self.assertTrue(all('2023-01-01' <= str(blog.created_at) <= '2023-12-31' for blog in filtered_queryset))
